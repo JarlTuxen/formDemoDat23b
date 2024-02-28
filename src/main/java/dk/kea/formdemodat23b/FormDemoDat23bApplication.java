9460ccabdd99ca85e7ee8049b64f6504bfaa9d0f
jarltuxen <jarl.tuxen@gmail.com>
2024-02-28 08:42:06 +0100

    Initial commit

---
+++ src/main/java/dk/kea/formdemodat23b/FormDemoDat23bApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.formdemodat23b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormDemoDat23bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormDemoDat23bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/kea/formdemodat23b/FormDemoDat23bApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.formdemodat23b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormDemoDat23bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
